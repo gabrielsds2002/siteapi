27d3df2f846c93716e61e3ec6d07cb858db83ac3
Gabriel Silveira <76741800+gabrielsds2002@users.noreply.github.com>
2021-09-23 19:18:29 -0300

    Add files via upload

--- apicep (1).js
+++ apicep (1).js
@@ -2,26 +2,18 @@ function buscaCep(url,body){
     let request = new XMLHttpRequest()
     let result  = ""
     request.open("POST", url,true)
-    request.sendRequestHeader("Content-type","application/json")
+    request.setRequestHeader("Content-type","application/json")
     request.send(JSON.stringify(body))
     request.onload = function(){
         console.log(this.responseText)
        result = JSON.parse(this.responseText)
-           if(this.status==200){
-
             montaHtmlSucesso(result);
-        }else{
-            montaHtmlErro(request);
-        }
        }
-      
-    
-
     return request.responseText
    }
 
    function enviarCep(){
-       let url ="https://apidecep.herokuapp.com/cep"
+       let url ="https://apicep2.herokuapp.com/cep"
        let cep = document.getElementById("cep").value
        body = {
            "cep": cep
@@ -42,7 +34,7 @@ function buscaCep(url,body){
         let html = "<h2> Resultado encontrado:  </h2>";
         console.log(result)
         html += "<ul>";
-        html += "<li><span style= 'fontWeight:bold'>Erro 400: CEP inválido </span> " + result.erro + "</li>"
+        html += "<li><span style= 'fontWeight:bold'>Erro 400: CEP invÃ¡lido </span> " + result.erro + "</li>"
     }
 
    window.onload = function(){
@@ -51,4 +43,3 @@ function buscaCep(url,body){
            enviarCep();
        });
    }
-
